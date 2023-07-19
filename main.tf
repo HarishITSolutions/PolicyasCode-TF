7ebd09bece2ae66f020ddcd1e14cc6ebcca2e930
Harish Nerella <503356053@ge.com>
2023-07-19 10:07:40 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -37,5 +37,5 @@ module "global_core" {
 
   environment           = "dev"
   initiative_definition = format("%s/initiatives/core.yaml", path.module)
-  initiative_definition = format("%s/initiatives/core2.yaml", path.module)
+  initiative_definition2 = format("%s/initiatives/core2.yaml", path.module)
 }
