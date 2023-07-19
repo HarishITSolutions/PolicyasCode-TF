93d5ff6803a6c9b4debbd3406178db77659a54df
Harish Nerella <503356053@ge.com>
2023-07-19 10:27:55 +0000

    Added 2 files to /modules/azure-policy-initiative2

---
+++ modules/azure-policy-initiative2/variables.tf
@@ -0,0 +1,67 @@
+variable "initiative_definition" {
+  type        = string
+  description = "Initiative Definition"
+}
+
+variable "environment" {
+  type        = string
+  description = "dev"
+}
+
+variable "assignment" {
+  type = object({
+    assignments = list(object({
+      id   = string
+      name = string
+    }))
+    scope = optional(string, "rg")
+  })
+  description = "Global Core Initiative"
+
+  validation {
+    condition = contains(
+      ["sub", "mg", "rg"],
+      var.assignment.scope
+    )
+    error_message = "Err: invalid assignment scope."
+  }
+}
+
+variable "exemptions" {
+  type = list(object({
+    id                   = string
+    risk_id              = string
+    scope                = string
+    category             = string
+    assignment_reference = string
+  }))
+  description = "Global Core Exemptions"
+
+  validation {
+    condition = alltrue(
+      [
+        for exemption in var.exemptions :
+        contains(
+          ["sub", "mg", "rg"],
+          exemption.scope
+        )
+      ]
+    )
+    error_message = "Err: invalid exemption scope."
+  }
+
+  validation {
+    condition = alltrue(
+      [
+        for exemption in var.exemptions :
+        contains(
+          ["Mitigated", "Waiver"],
+          exemption.category
+        )
+      ]
+    )
+    error_message = "Err: invalid exemption category."
+  }
+
+  default = []
+}
